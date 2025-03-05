4a63b38347dbc9abf33c380111c9406897188fc7
paperweight2012 <ppwt@rocketmail.com>
2025-03-04 20:13:08 -0800

    added about me

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -10,10 +10,10 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
-	author: 'Your Name', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	site: 'https://www.nagaswamy.com', // Write here your website url
+	author: 'Venkat Nagaswamy', // Site author
+	title: "Venkat's Blog", // Site title.
+	description: 'Well, the name says it all. It is my blog',
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
